0749a32eb61e19406cc3ee679964257a47e2c0ad
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-06 00:57:00 -0300

    Add project files.

---
+++ Impar_Par/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int digito;
+
+        Console.WriteLine("Informe o número: ");
+        digito = int.Parse(Console.ReadLine());
+        
+        if(digito % 2 == 0)
+        {
+            Console.WriteLine("O número é par!");
+
+        }
+        else
+        {
+            Console.WriteLine("O número é impar!");
+        }
+    }
+}
